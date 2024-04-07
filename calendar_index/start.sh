8083f2e86a3b80d82c68bf69a406fb80dbba0ff2
Damian Akpan <damiakpan@gmail.com>
2024-04-07 02:47:26 +0000

    Quiet prefect logs
    Signed-off-by: Damian Akpan <damiakpan@gmail.com>

--- calendar_index/start.sh
+++ calendar_index/start.sh
@@ -9,11 +9,11 @@ if [ -n "$FLY_APP_NAME" ]; then
   echo 10 > /proc/sys/vm/swappiness
   swapon /swapfile
 
-  /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+  /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &>/dev/null &
   /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=fly-app
 else
   echo "Not running in fly.io"
-  /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+  /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &>/dev/null &
   /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app
   echo Tailscale started
   export ALL_PROXY=socks5://localhost:1055
