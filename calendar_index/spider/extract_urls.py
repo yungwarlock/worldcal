389e3ce8c41e1346c564dfcb5014c292859ae549
Damian Akpan <damiakpan@gmail.com>
2024-04-10 09:04:45 +0000

    Implemented initial version of spider
    Signed-off-by: Damian Akpan <damiakpan@gmail.com>

---
+++ calendar_index/spider/extract_urls.py
@@ -0,0 +1,66 @@
+import re
+import requests
+from multiprocessing.pool import ThreadPool
+
+import numpy as np
+from prefect import task
+from bs4 import BeautifulSoup
+
+
+@task
+def extract_all_urls(url, max_depth=2):
+    all_store = np.array([])
+
+    def process_url(url):
+        res = set()
+        doc = requests.get(url)
+        soup = BeautifulSoup(doc.text, 'html.parser')
+        for link in soup.find_all("a"):
+            if not link.get("href", None):
+                continue
+            is_not_in_store = link["href"] not in all_store
+            is_valid_url = re.fullmatch(r'\bhttps?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*', link["href"])
+            has_base_path = link["href"].find(url) == 0
+            if is_not_in_store and is_valid_url and has_base_path:
+                res.add(link["href"])
+        return list(res)
+
+    prev = np.array([url])
+    while max_depth > 0:
+        with ThreadPool() as pool:
+            results = pool.map(process_url, prev)
+            all_store = np.concatenate((all_store, *results), axis=None)
+            prev = np.concatenate(([], *results), axis=None)
+        max_depth -= 1
+
+    return all_store
+
+
+@task
+def extract_all_urls2(url):
+    # This function is a simplified version of the get_all_links function from the extract_urls.py file
+    # It uses a simple loop to extract all the URLs from the given URL
+    all_urls = set()
+    urls_to_process = [url]
+    while urls_to_process:
+        current_url = urls_to_process.pop()
+        if current_url in all_urls:
+            continue
+        doc = requests.get(current_url)
+        soup = BeautifulSoup(doc.text, 'html.parser')
+        for link in soup.find_all("a"):
+            if not link.get("href", None):
+                continue
+            is_valid_url = re.fullmatch(r'\bhttps?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*', link["href"])
+            has_base_path = link["href"].find(url) == 0
+            if is_valid_url and has_base_path:
+                all_urls.add(link["href"])
+                urls_to_process.append(link["href"])
+    return list(all_urls)
+
+@task
+def batch_array(arr, batch_size=4):
+    items = []
+    for i in range(0, len(arr), batch_size):
+        items.append(arr[i: i + batch_size])
+    return items
---
+++ calendar_index/spider/spider.py
@@ -0,0 +1,27 @@
+from prefect import flow
+from prefect.artifacts import create_table_artifact
+
+from extract_urls import extract_all_urls, batch_array
+
+@flow
+def spider(url):
+    all_res = extract_all_urls(url)
+    batch_links = batch_array(list(all_res), batch_size=10)
+
+    create_table_artifact(
+        key="all links",
+        table=batch_links,
+        description=f"""
+All links from the page
+
+Page: {url}
+links count: {len(all_res)}
+links: {batch_links}
+""",
+    )
+    return batch_links
+
+if __name__ == "__main__":
+    spider.serve(
+        name="get_page_events"
+    )
