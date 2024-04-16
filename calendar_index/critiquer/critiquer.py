a18c594419a0fe80937b1893901f7534b72343a8
Damian <damiakpan@gmail.com>
2024-04-16 02:26:13 +0000

    Fixed services not serving
    Signed-off-by: Damian <damiakpan@gmail.com>

--- calendar_index/critiquer/critiquer.py
+++ calendar_index/critiquer/critiquer.py
@@ -8,3 +8,16 @@ def critique_event(event: str):
 
     res = critiquer.critique(event)
     return res
+
+if __name__ == "__main__":
+    critique_event.serve(name="critique_event")
+#     res = critique_event(
+#         """
+# cameron vs nigeria 2 - 0
+
+# Day: 27
+# Month: 6
+# Year: 0
+# """
+#     )
+#     print("res:", res)
