4c4a5eeda93c4a81663dd063e2f16eb1c33ce7ab
Damian <damiakpan@gmail.com>
2024-04-18 13:36:41 +0100

    Added exception handler for spider extract_url if url does not respond

--- calendar_index/spider/extract_urls.py
+++ calendar_index/spider/extract_urls.py
@@ -16,21 +16,24 @@ def extract_all_urls(url, json_writer: JSONLManager, max_depth=2):
 
     def process_url(url: str):
         res = set()
-        doc = requests.get(url)
-        soup = BeautifulSoup(doc.text, "html.parser")
+        try:
+            doc = requests.get(url)
+            soup = BeautifulSoup(doc.text, "html.parser")
 
-        for link in soup.find_all("a"):
-            if not link.get("href", None):
-                continue
-            is_not_in_store = bloom_filter.lookup(link["href"]) is False
-            is_valid_url = re.fullmatch(
-                r"\bhttps?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*", link["href"]
-            )
-            # has_base_path = link["href"].find(url) == 0
-            is_not_banned_url = check_is_banned_url(link["href"]) is False
-            if is_not_in_store and is_valid_url and is_not_banned_url:
-                bloom_filter.add(link["href"])
-                res.add(link["href"])
+            for link in soup.find_all("a"):
+                if not link.get("href", None):
+                    continue
+                is_not_in_store = bloom_filter.lookup(link["href"]) is False
+                is_valid_url = re.fullmatch(
+                    r"\bhttps?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*", link["href"]
+                )
+                # has_base_path = link["href"].find(url) == 0
+                is_not_banned_url = check_is_banned_url(link["href"]) is False
+                if is_not_in_store and is_valid_url and is_not_banned_url:
+                    bloom_filter.add(link["href"])
+                    res.add(link["href"])
+        except Exception:
+            return res
 
         data = []
         for link in res:
