2eb97377ec5a7d701d5298cef99a6ad6b943cdd6
Damian Akpan <damiakpan@gmail.com>
2024-04-21 09:36:27 +0100

    Updated interval of muncher to per hour

--- calendar_index/muncher/muncher.py
+++ calendar_index/muncher/muncher.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
         name="get_events",
         schedule=IntervalSchedule(
             timezone="America/Chicago",
-            interval=timedelta(minutes=10),
+            interval=timedelta(hours=1),
             anchor_date=datetime(2024, 3, 1, 0, 0),
         ),
     )  # type: ignore
