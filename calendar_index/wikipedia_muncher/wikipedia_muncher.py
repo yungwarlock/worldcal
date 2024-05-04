f1cd6030ca012d9a2e9a7d7f1943687a2da7377b
Damian <damiakpan@gmail.com>
2024-05-04 10:39:02 +0000

    Increased job interval to 50 minutes. Added debug env handling
    Signed-off-by: Damian <damiakpan@gmail.com>

--- calendar_index/wikipedia_muncher/wikipedia_muncher.py
+++ calendar_index/wikipedia_muncher/wikipedia_muncher.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta, datetime
 
 from prefect import flow, get_run_logger
@@ -5,6 +6,11 @@ from prefect.client.schemas.schedules import IntervalSchedule
 
 from extract_events import extract_events
 from database import extract_batch, save_batch_to_database, set_url_as_failed
+
+
+DEBUG = os.environ.get("DEBUG", "") is True
+JOB_INTERVAL = int(os.environ.get("JOB_INTERVAL", 50))
+
 
 @flow
 def wikipedia_muncher():
@@ -25,11 +31,14 @@ def wikipedia_muncher():
 
 
 if __name__ == "__main__":
-    wikipedia_muncher.serve(
-        name="wikipedi_muncher",
-        schedule=IntervalSchedule(
-            timezone="America/Chicago",
-            interval=timedelta(minutes=10),
-            anchor_date=datetime(2024, 3, 1, 0, 0),
-        ),
-    )  # type: ignore
+    if DEBUG:
+        wikipedia_muncher()
+    else:
+        wikipedia_muncher.serve(
+            name="wikipedi_muncher",
+            schedule=IntervalSchedule(
+                timezone="America/Chicago",
+                interval=timedelta(minutes=JOB_INTERVAL),
+                anchor_date=datetime(2024, 3, 1, 0, 0),
+            ),
+        )  # type: ignore
